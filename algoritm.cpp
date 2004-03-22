03723744f7fe7efc94688538e5665e05ff705c20
peter <peter@50aeb178-8500-dd11-b8ad-001a4d4a7278>
2004-03-22 20:08:17 +0000

    This commit was generated by cvs2svn to compensate for changes in r6464, which included commits to RCS files with non-trunk default branches.

---
+++ algoritm.cpp
@@ -0,0 +1,47 @@
+
+
+#include "..\msp\type_def.h"
+#include "algoritm.h"
+// Коэфициенты ОКОН
+
+const s16 WIN_BL[]={417, 4233, 16655, 30465, 30465, 16655, 4233, 417};
+const s16 WIN_BL_MINUS[]={-417,-4233,-16655,-30465,-30465,-16655,-4233,-417};
+const s16 WIN_BL_075[]={ 313, 3175, 12491, 22849, 22849, 12491, 3175, 313};
+const s16 WIN_BL_025[]={ 104, 1058,  4164,  7616,  7616,  4164, 1058, 104};
+
+//требуемая ширина для обсчета 20 отсчетов
+
+s16 black_men8_12(s16* inp){
+ long t=0;
+
+ for (int x=0;x<8;x++){
+  t+=((long)(*inp++))*WIN_BL_MINUS[x];
+  }
+ inp+=4;
+ for (int x=0;x<8;x++){
+  t+=((long)(*inp++))*WIN_BL[x];
+  }
+ return t/32768;
+}
+
+
+//требуемая ширина для обсчета 24 отсчета
+s16 zykov_man_4_12_blackman(s16* inp){
+ double long t=0;
+ s16* inp1=inp;
+ for (int x=0;x<8;x++){
+  t+=((long)(*inp++))*WIN_BL_075[x];
+  }
+
+ inp=inp1+4;
+ for (int x=0;x<8;x++){
+  t+=((long)(*inp++))*WIN_BL_MINUS[x];
+  }
+
+ inp=inp1+16;
+ for (int x=0;x<8;x++){
+  t+=((long)(*inp++))*WIN_BL_025[x];
+  }
+
+ return t/32768;
+}
---
+++ algoritm.h
@@ -0,0 +1,3 @@
+s16 black_men8_12(s16* inp);
+s16 zykov_man_4_12_blackman(s16* inp);
+
